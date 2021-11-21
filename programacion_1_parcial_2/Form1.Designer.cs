146eb46324fe58dc51972a777efc4b5b14ad8bbf
olmenies <olmedonies@gmail.com>
2021-11-21 17:11:08 -0300

    Add project files.

---
+++ programacion_1_parcial_2/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace programacion_1_parcial_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxCheckBox = new System.Windows.Forms.GroupBox();
+            this.checkBoxFacturasPares = new System.Windows.Forms.CheckBox();
+            this.checkBoxFacturasImpares = new System.Windows.Forms.CheckBox();
+            this.checkBoxMayoresIguales10k = new System.Windows.Forms.CheckBox();
+            this.checkBoxMenores10k = new System.Windows.Forms.CheckBox();
+            this.buttonConsultar = new System.Windows.Forms.Button();
+            this.buttonSalir = new System.Windows.Forms.Button();
+            this.listBoxFacturas = new System.Windows.Forms.ListBox();
+            this.groupBoxCheckBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxCheckBox
+            // 
+            this.groupBoxCheckBox.Controls.Add(this.checkBoxMenores10k);
+            this.groupBoxCheckBox.Controls.Add(this.checkBoxMayoresIguales10k);
+            this.groupBoxCheckBox.Controls.Add(this.checkBoxFacturasImpares);
+            this.groupBoxCheckBox.Controls.Add(this.checkBoxFacturasPares);
+            this.groupBoxCheckBox.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxCheckBox.Name = "groupBoxCheckBox";
+            this.groupBoxCheckBox.Size = new System.Drawing.Size(602, 66);
+            this.groupBoxCheckBox.TabIndex = 0;
+            this.groupBoxCheckBox.TabStop = false;
+            this.groupBoxCheckBox.Text = "Seleccionar filtro";
+            // 
+            // checkBoxFacturasPares
+            // 
+            this.checkBoxFacturasPares.AutoSize = true;
+            this.checkBoxFacturasPares.Location = new System.Drawing.Point(6, 16);
+            this.checkBoxFacturasPares.Name = "checkBoxFacturasPares";
+            this.checkBoxFacturasPares.Size = new System.Drawing.Size(96, 17);
+            this.checkBoxFacturasPares.TabIndex = 0;
+            this.checkBoxFacturasPares.Text = "Facturas pares";
+            this.checkBoxFacturasPares.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFacturasImpares
+            // 
+            this.checkBoxFacturasImpares.AutoSize = true;
+            this.checkBoxFacturasImpares.Location = new System.Drawing.Point(6, 39);
+            this.checkBoxFacturasImpares.Name = "checkBoxFacturasImpares";
+            this.checkBoxFacturasImpares.Size = new System.Drawing.Size(106, 17);
+            this.checkBoxFacturasImpares.TabIndex = 1;
+            this.checkBoxFacturasImpares.Text = "Facturas impares";
+            this.checkBoxFacturasImpares.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxMayoresIguales10k
+            // 
+            this.checkBoxMayoresIguales10k.AutoSize = true;
+            this.checkBoxMayoresIguales10k.Location = new System.Drawing.Point(297, 39);
+            this.checkBoxMayoresIguales10k.Name = "checkBoxMayoresIguales10k";
+            this.checkBoxMayoresIguales10k.Size = new System.Drawing.Size(162, 17);
+            this.checkBoxMayoresIguales10k.TabIndex = 2;
+            this.checkBoxMayoresIguales10k.Text = "Mayores o iguales a $10.000";
+            this.checkBoxMayoresIguales10k.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxMenores10k
+            // 
+            this.checkBoxMenores10k.AutoSize = true;
+            this.checkBoxMenores10k.Location = new System.Drawing.Point(297, 16);
+            this.checkBoxMenores10k.Name = "checkBoxMenores10k";
+            this.checkBoxMenores10k.Size = new System.Drawing.Size(118, 17);
+            this.checkBoxMenores10k.TabIndex = 3;
+            this.checkBoxMenores10k.Text = "Menores a $10.000";
+            this.checkBoxMenores10k.UseVisualStyleBackColor = true;
+            // 
+            // buttonConsultar
+            // 
+            this.buttonConsultar.Location = new System.Drawing.Point(12, 84);
+            this.buttonConsultar.Name = "buttonConsultar";
+            this.buttonConsultar.Size = new System.Drawing.Size(291, 23);
+            this.buttonConsultar.TabIndex = 1;
+            this.buttonConsultar.Text = "Consultar";
+            this.buttonConsultar.UseVisualStyleBackColor = true;
+            this.buttonConsultar.Click += new System.EventHandler(this.buttonConsultar_Click);
+            // 
+            // buttonSalir
+            // 
+            this.buttonSalir.Location = new System.Drawing.Point(309, 84);
+            this.buttonSalir.Name = "buttonSalir";
+            this.buttonSalir.Size = new System.Drawing.Size(305, 23);
+            this.buttonSalir.TabIndex = 2;
+            this.buttonSalir.Text = "Salir";
+            this.buttonSalir.UseVisualStyleBackColor = true;
+            this.buttonSalir.Click += new System.EventHandler(this.buttonSalir_Click);
+            // 
+            // listBoxFacturas
+            // 
+            this.listBoxFacturas.FormattingEnabled = true;
+            this.listBoxFacturas.Location = new System.Drawing.Point(12, 113);
+            this.listBoxFacturas.Name = "listBoxFacturas";
+            this.listBoxFacturas.Size = new System.Drawing.Size(602, 225);
+            this.listBoxFacturas.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 358);
+            this.Controls.Add(this.listBoxFacturas);
+            this.Controls.Add(this.buttonSalir);
+            this.Controls.Add(this.buttonConsultar);
+            this.Controls.Add(this.groupBoxCheckBox);
+            this.Name = "Form1";
+            this.Text = "Programacion 1 - Parcial 2";
+            this.groupBoxCheckBox.ResumeLayout(false);
+            this.groupBoxCheckBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxCheckBox;
+        private System.Windows.Forms.CheckBox checkBoxMenores10k;
+        private System.Windows.Forms.CheckBox checkBoxMayoresIguales10k;
+        private System.Windows.Forms.CheckBox checkBoxFacturasImpares;
+        private System.Windows.Forms.CheckBox checkBoxFacturasPares;
+        private System.Windows.Forms.Button buttonConsultar;
+        private System.Windows.Forms.Button buttonSalir;
+        private System.Windows.Forms.ListBox listBoxFacturas;
+    }
+}
+
---
+++ programacion_1_parcial_2/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //Importamos esto para poder trabajar con archivos
+
+namespace programacion_1_parcial_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //path al directorio donde guardo mis files
+        string DATA_PATH = @"Q:\Documents\UAI\programacion_1\programacion_1_parcial_2\programacion_1_parcial_2\data\"; //Directorio donde vamos a guardar los archivos
+
+        //evento buttonConsultar_Click para consultar con los archivos la información solicitada
+        private void buttonConsultar_Click(object sender, EventArgs e)
+        {
+            listBoxFacturas.Items.Clear();
+            bool pares, impares, menores10k, mayoresIguales10k;
+            pares = impares = menores10k = mayoresIguales10k = false;
+
+            if (checkBoxFacturasPares.Checked)
+                pares = true;
+
+            if (checkBoxFacturasImpares.Checked)
+                impares = true;
+
+            if (checkBoxMenores10k.Checked)
+                menores10k = true;
+
+            if (checkBoxMayoresIguales10k.Checked)
+                mayoresIguales10k = true;
+
+            //llamado a la función para imprimir las facturas según los filtros seleccionados
+            ImprimirFacturas(pares, impares, menores10k, mayoresIguales10k);
+        }
+
+        //método ImprimirFacturas para imprimir las facturas en el listbox según los filtros seleccinoados
+        private void ImprimirFacturas(bool _pares, bool _impares, bool _menores10k, bool _mayoresIguales10k)
+        {
+
+            string pathEncabezado = DATA_PATH + "encabezado.txt";
+            FileStream archivoEncabezado = new FileStream(pathEncabezado, FileMode.Open);
+            StreamReader lectorEncabezado = new StreamReader(archivoEncabezado);
+
+            string[] vectorRegistrosEncabezado = new string[4];
+            string[] vectorRegistrosDetalle = new string[5];
+            string registroDetalle;
+            string registroEncabezado;
+
+            int facturaAcumulador = 0;
+            int acumulador = 0;
+
+            string facturaActual = "1001"; //primer factura
+
+
+            while (lectorEncabezado.Peek() > -1) //while para recorrer el archivo de encabezados
+            {
+                registroEncabezado = lectorEncabezado.ReadLine();
+                vectorRegistrosEncabezado = registroEncabezado.Split(';');
+
+                //aplicamos el filtro para mostrar solo las facturas pares
+                if((_pares && !_impares) && (Convert.ToInt32(facturaActual) % 2 != 0))
+                {
+                    facturaActual = Convert.ToString(Convert.ToInt32(facturaActual) + 1);
+                    continue;
+                }//fin del if
+
+                //aplicamos el filtro para mostrar solo las facturas impares
+                else if((!_pares && _impares) && (Convert.ToInt32(facturaActual)%2 == 0))
+                {
+                    facturaActual = Convert.ToString(Convert.ToInt32(facturaActual) + 1);
+                    continue;
+                }//fin del if
+
+                //aplicamos el filtro para nm
+                else if((!_pares && !_impares))
+                {
+                    break;
+                }
+
+                if (vectorRegistrosEncabezado[0] == facturaActual)
+                {
+                    
+                    listBoxFacturas.Items.Add("******************************************************************************************************");
+                    listBoxFacturas.Items.Add("Factura Nro: " + vectorRegistrosEncabezado[0] + "\t Fecha: " + vectorRegistrosEncabezado[3] + "Nombre; " + vectorRegistrosEncabezado[2]);
+                    listBoxFacturas.Items.Add("***");
+                    
+                    string pathDetalle = DATA_PATH + "detalle.txt";
+                    FileStream archivoDetalle = new FileStream(pathDetalle, FileMode.Open);
+                    StreamReader lectorDetalle = new StreamReader(archivoDetalle);
+
+                    while (lectorDetalle.Peek() > -1)
+                    {
+                        registroDetalle = lectorDetalle.ReadLine();
+                        vectorRegistrosDetalle = registroDetalle.Split(';');
+
+                        if (vectorRegistrosEncabezado[0] == vectorRegistrosDetalle[0]) //apareo de encabezado.txt y detalle.txt
+                        {
+                            acumulador = acumulador + 1;
+                            facturaAcumulador += Convert.ToInt32(vectorRegistrosDetalle[3]) * Convert.ToInt32(vectorRegistrosDetalle[4]);
+                            listBoxFacturas.Items.Add(vectorRegistrosDetalle[1] + "\t" + vectorRegistrosDetalle[2] + "\t" + vectorRegistrosDetalle[3] + "\t" + vectorRegistrosDetalle[4]);
+                        }
+                    }
+
+                    lectorDetalle.Close();
+                    archivoDetalle.Close();
+                }
+                
+                facturaActual = Convert.ToString(Convert.ToInt32(facturaActual) + 1);
+                listBoxFacturas.Items.Add("***");
+                listBoxFacturas.Items.Add("Total: " + facturaAcumulador);
+
+                int lineasListbox = listBoxFacturas.Items.Count - 1;
+
+                //borramos las facturas que no cumplan con el filtro seleccionado
+                if((_menores10k && !_mayoresIguales10k) && facturaAcumulador>=10000)
+                {
+                    for (int i = lineasListbox; i >= (lineasListbox - 4 - acumulador); i--)
+                        listBoxFacturas.Items.RemoveAt(i);
+                }
+                
+                //borramos la factura que no cumpla con el filtro seleccionado
+                else if((!_menores10k && _mayoresIguales10k) && facturaAcumulador<=10000)
+                {
+                    for (int i = lineasListbox; i >= (lineasListbox - 4 - acumulador); i--)
+                        listBoxFacturas.Items.RemoveAt(i);
+                }
+
+                //borramos todas las facturas
+                else if(!_menores10k && !_mayoresIguales10k)
+                {
+                    listBoxFacturas.Items.Clear();
+                    break;
+                }
+
+                facturaAcumulador = 0;
+                acumulador = 0;
+            }
+            lectorEncabezado.Close();
+            archivoEncabezado.Close();
+        }
+
+        //inicio del evento buttonSalir_Click para salir de la aplicación cuando se clickeé el botón
+        private void buttonSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }//fin del evento buttonSalir_Click()
+    }
+}
